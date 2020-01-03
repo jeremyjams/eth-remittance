6108900c074a2d846dc78e0e80617b0b466d91bd
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2020-01-03 16:20:20 +0100

    Claimable date

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,19 +1,23 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-//import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/lifecycle/Pausable.sol";//TODO move *.sol to current folder
 
-contract Remittance is Pausable {
+contract Remittance is Ownable, Pausable {
 
     bytes32 private redeemSecretHash;
+    uint private claimableStartDate;
 
     event RedeemEvent(address recipient, uint amount);
+    event ClaimEvent(address recipient, uint amount);
 
-    constructor(bytes32 _redeemSecretHash) Pausable() public payable {
+    constructor(bytes32 _redeemSecretHash, uint8 claimableHoursAfter) Pausable() public payable {
         require(msg.value > 0, "Funds required");
         require(_redeemSecretHash != 0, "Empty RedeemSecretHash"); //prevents badly formatted construction
+        require(claimableHoursAfter < 10 days, "Empty RedeemSecretHash"); //prevents badly formatted construction
 
         redeemSecretHash = _redeemSecretHash;
+        claimableStartDate = now + claimableHoursAfter * 1 hours;
     }
 
     /*
@@ -25,12 +29,27 @@ contract Remittance is Pausable {
         bytes32 secretHash = keccak256(abi.encodePacked(secret1, secret2));
         require(secretHash == redeemSecretHash, "Unauthorized to redeem");
 
-        emit RedeemEvent(msg.sender, address(this).balance); //is address(this).balance expensive?
+        emit RedeemEvent(msg.sender, address(this).balance);
+        require(withdraw(), "Redeem transfer failed");
+    }
+
+    function withdraw() private returns (bool success) {
         (success,) = msg.sender.call.value(address(this).balance)("");
-        require(success, "Redeem transfer failed");
+        require(success, "Withdraw failed");
+    }
+
+    function claim() public onlyOwner onlyAfter(claimableStartDate) returns (bool success) {
+        require(address(this).balance > 0, "Nothing to claim");
+
+        emit ClaimEvent(msg.sender, address(this).balance);
+        require(withdraw(), "Redeem transfer failed");
+    }
+
+    modifier onlyAfter(uint time) {
+        require(now >= time, "Please wait");
+        _;
     }
 
     function() external payable { }
-
 
 }
